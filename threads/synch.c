c4ebe81840058bc05912225aa198853302d8a574
jihyun <cat2998@naver.com>
2024-04-29 13:01:14 +0000

    test
    Co-authored-by: dylanYim <dongkyun94@gmail.com>
    Co-authored-by: youngmanpark <py980627@google.com>

--- threads/synch.c
+++ threads/synch.c
@@ -64,7 +64,8 @@ void sema_down(struct semaphore *sema) {
 
     old_level = intr_disable();
     while (sema->value == 0) {
-        list_push_back(&sema->waiters, &thread_current()->elem);
+        list_insert_ordered(&sema->waiters, &thread_current()->elem, compare_priority, NULL);
+        // list_push_back(&sema->waiters, &thread_current()->elem);
         thread_block();
     }
     sema->value--;
